c3afeb3747708e2cc1d98820bf33c0b4da38c1bb
shadowmoonezios <agushifelde@hotmail.com>
2025-08-31 11:26:35 +0300

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mein Rezeptsammler</h1>
+      <p>Willkommen zu deiner Rezeptverwaltung!</p>
+    </div>
+  );
+}
+
+export default App;
